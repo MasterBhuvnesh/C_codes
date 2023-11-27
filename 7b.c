2f7aaa8c2af0ad11f210fbb1378f869f9a2ceed1
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2023-11-27 13:10:37 +0530

    Update 7b.c
    It has 8b in it

--- 7b.c
+++ 7b.c
@@ -31,3 +31,51 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    // Open the file in read mode
+    FILE *file = fopen("your_file.txt", "r");
+
+    // Check if the file opened successfully
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1; // Exit with an error code
+    }
+
+    // Initialize counters
+    int vowelCount = 0, consonantCount = 0, digitCount = 0, spaceCount = 0;
+    char ch;
+
+    // Read each character from the file
+    while ((ch = fgetc(file)) != EOF) {
+        // Check if the character is a vowel
+        if (strchr("aeiouAEIOU", ch) != NULL) {
+            vowelCount++;
+        }
+        // Check if the character is a consonant
+        else if (isalpha(ch)) {
+            consonantCount++;
+        }
+        // Check if the character is a digit
+        else if (isdigit(ch)) {
+            digitCount++;
+        }
+        // Check if the character is a space
+        else if (isspace(ch)) {
+            spaceCount++;
+        }
+    }
+
+    // Close the file
+    fclose(file);
+
+    // Display the counts
+    printf("Number of vowels: %d\n", vowelCount);
+    printf("Number of consonants: %d\n", consonantCount);
+    printf("Number of digits: %d\n", digitCount);
+    printf("Number of spaces: %d\n", spaceCount);
+
+    return 0; // Exit successfully
+}
